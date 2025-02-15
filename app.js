ffcac9faecc09f27aaf04d636ee9d4789ab241a8
prasad17 <prasadgujarathi619@gmail.com>
2025-02-15 22:13:56 +0530

    Added Arcjet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import { PORT } from './config/env.js';
 import connectToDatabase from './database/mongodb.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionsRouter from './routes/subscription.route.js';
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : false}));
 
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', usersRouter);
